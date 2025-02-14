40893e8c1ec800a69c4edf4fa1ed9c9e231e9109
C̴̄̂ä̴́͗t̴̓̑ <151648110+thatgreyCat7777@users.noreply.github.com>
2025-02-14 09:10:55 +0800

    Added comment on better IsWall function

--- Source/Movement_Remake/FPSCharacter.cpp
+++ Source/Movement_Remake/FPSCharacter.cpp
@@ -230,5 +230,6 @@ void AFPSCharacter::GradualCrouch(float ZScale, const float &DeltaTime)
 
 bool AFPSCharacter::IsWall(const FVector &Normal)
 {
+    // return Fmath::IsNearlyEqual(Fmath::Abs(Normal.Z), 0)
     return FMath::IsNearlyEqual(FMath::Abs(Normal.X), 1) || FMath::IsNearlyEqual(FMath::Abs(Normal.Y), 1);
 }
